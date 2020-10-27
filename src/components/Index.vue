a874f6ac6c4195ea37a965fb2465f5295f241dc4
Rockin <rockin.yzq@outlook.com>
2020-10-28 01:11:35 +0900

    20201026_router模式原理

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,12 +1,30 @@
 <template>
   <div id="index">
     这是首页组件
+    <div v-if="name">从about页面传过来的值{{name}}</div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Index"
+  name: "Index",
+  data(){
+    return{
+      name:undefined
+    }
+  },
+  mounted(){
+    console.log('home组件mount')
+    this.name = this.$route.query.name
+  },
+  watch:{
+    $route(to,from){
+      console.log('watch back')
+      this.$router.go(0)//组件重新加载
+      //虽然底层的代码是后退，但是后退里面添加了初始化代码，保证数据重新加载
+      //防止访问旧数据
+    }
+  }
 }
 </script>
 
